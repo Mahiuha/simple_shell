77d49090e8d2794f8417bc46e9b3dbde37359627
lew-monk <lewmonk@gmail.com>
2021-08-22 10:59:48 +0300

    Fixed err log

--- builtins_help_2.c
+++ builtins_help_2.c
@@ -31,7 +31,7 @@ void help_setenv(void)
 
 /**
  * help_unsetenv - Displays information on the shellby builtin command
- * 'unsetenv'.
+ * 'unsetenv' as a better implementation.
  */
 void help_unsetenv(void)
 {
