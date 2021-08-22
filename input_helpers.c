4ced5eab517c0b27367466fa2c2fa692450298c3
lew-monk <lewmonk@gmail.com>
2021-08-22 11:11:26 +0300

    Improved Efficieny for the Algorithm

--- input_helpers.c
+++ input_helpers.c
@@ -14,7 +14,7 @@ int check_args(char **args);
 
 /**
  * get_args - Gets a command from standard input.
- * @line: A buffer to store the command.
+ * @line: A buffer to store the command to improve perfomance.
  * @exe_ret: The return value of the last executed command.
  *
  * Return: If an error occurs - NULL.
